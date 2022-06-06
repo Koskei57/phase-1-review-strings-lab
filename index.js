0b0d6d1a84707701eda4429d6be5ca20b8a55cca
Koskei57 <dennis.koskei@student.moringaschool.com>
2022-06-06 18:51:53 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'is defined';
+const welcomeMessage = `"Welcome to Flatbook, " ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+const upperCaseCurrentUser = currentUser.toUpperCase();
+const shortGreeting = `Welcome, ${ firstInitial }!`;
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
